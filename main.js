ddf435149fc22661a16f771e070cdc845f11d64e
RacMcT <raccmct@gmail.com>
2021-03-11 20:53:11 -0600

    started GUI

--- main.js
+++ main.js
@@ -1,35 +1,25 @@
-'use strict';
+// 'use strict';
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// // brings in the assert module for unit testing
+// const assert = require('assert');
+// // brings in the readline module to access the command line
+// const readline = require('readline');
+// // use the readline module to print out to the command line
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 
-const pigLatin = (word) => {
-
-  // Your code here
-  //Turns variable "word" into an array (wordArr- each letter seperate - OR KEEP AS A STRING?
-  // let wordArr = word.split('')
-  
-  //Trims off whitespace at ends and all lowercase for input word (string)
+const pigLatin = () => {
+  let word = document.getElementById("input").value
   word= word.toLowerCase().trim();
-  // word = str.split(" "); -- fails test so need to work out the error... new variable name?
-
-  //ARE EDGE CASES OF MULTIPLE WORDS REQUIRED OR PUSH MYSELF??
-  
-  // Make array of vowels to reference
   const vowels = ["a", "e", "i", "o", "u"];
 
-  // Set piglatin word to hold new word for output?
-  let pigWord="";
+// holds new word for output
+  let pigWord=""; 
 
-  //If it doesn't start with a vowel than by default it starts with a constanant- so simple if/else statement?
+  //If it doesn't start with a vowel than by default it starts with a constanant- if/else statement
 
   if(vowels.indexOf(word[0]) > -1){
     pigWord = word + "yay";
@@ -38,60 +28,50 @@ const pigLatin = (word) => {
     let firstMatch = word.match(/[aeiou]/g) || 0;
     let vowels = word.indexOf (firstMatch [0]);
     pigWord = word.substring(vowels) + word.substring(0,vowels) + "ay";
-    return pigWord;
+    return document.getElementById("translation").innerHTML += pigWord; 
   }
 
+  
   }
-
-  //****************WORK ON THIS WEEK BEFORE WEDNESDAY CLASS ****************
-// IF USER ENTERS MULTIPLE WORDS- HOW TO THEN TAKE THEM, SEPERATE INTO DIFFERENT WORDS AND THEN RUN THROUGH MY FUNCTION?!?!
-//***************************************************************************
-
-// split() to split string into an array of substrings, returns new array.
-// then need to loop through array of substrings and apply same conditional logic as code above
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
-//let word = str.split(" ");
-//
-
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
-const getPrompt = () => {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
-  });
-}
+// const getPrompt = () => {
+//   rl.question('word ', (answer) => {
+//     console.log( pigLatin(answer) );
+//     getPrompt();
+//   });
+// }
 
-// Unit Tests
-// to use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
+// // Unit Tests
+// // to use them run the command: npm test main.js
+// // to close them ctrl + C
+// if (typeof describe === 'function') {
 
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-  });
-} else {
+//   describe('#pigLatin()', () => {
+//     it('should translate a simple word', () => {
+//       assert.equal(pigLatin('car'), 'arcay');
+//       assert.equal(pigLatin('dog'), 'ogday');
+//     });
+//     it('should translate a complex word', () => {
+//       assert.equal(pigLatin('create'), 'eatecray');
+//       assert.equal(pigLatin('valley'), 'alleyvay');
+//     });
+//     it('should attach "yay" if word begins with vowel', () => {
+//       assert.equal(pigLatin('egg'), 'eggyay');
+//       assert.equal(pigLatin('emission'), 'emissionyay');
+//     });
+//     it('should lowercase and trim word before translation', () => {
+//       assert.equal(pigLatin('HeLlO '), 'ellohay');
+//       assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+//     });
+//   });
+// } else {
 
-  getPrompt();
+//   getPrompt();
 
-}
+// }
 
 
 // **********
